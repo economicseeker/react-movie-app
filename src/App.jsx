96caddc8d0fb0c2e67bf9637500f8a1bfb5aac28
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2025-08-04 22:18:59 +0300

    API_KEY env file redeclared and endpoint typo is fixed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Search from './components/Search.jsx'
+
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later.');
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
 
@@ -12,9 +52,15 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1> Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <p>Search</p>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
 
     </main>
